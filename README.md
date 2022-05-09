1b34b4557c0440f52a756b747f8ab0a99c6a5bc7
Ashutosh Samal - a0s0iro <Ashutosh.Samal@walmartlabs.com>
2022-05-09 20:46:12 +0530

    readme file

--- README.md
+++ README.md
@@ -1,13 +1,19 @@
 # DATA ENGINEERING ON YELP DATASET
 
 
-#PURPOSE 
+
 
 
 
 #SUMMERY
+
 This is an end to end data engineering project for YELP data set(https://www.yelp.com/dataset/documentation/main)
-that I build to get an hands on experience on spark and hadoop ecosystem.
-I have used scala as a programing language , spark as compute engine and hive 
+that I build to get a hands-on experience on spark and hadoop ecosystem.
+I have used scala as a programing language , spark as compute engine and hive as warehouse and
+used data proc cluster in GCP to deploy my code. 
+
+# DATASET
+
+
 
 
---
+++ target/scala-2.12/classes/DDL/customer_facing_table/user.sql
@@ -0,0 +1,36 @@
+DROP TABLE IF EXISTS yelp_dataset_etl.user_dim;
+
+CREATE TABLE IF NOT EXISTS yelp_dataset_etl.user_dim
+(
+user_table_pk string,
+user_name string,
+reviews_count integer,
+joined_date date,
+useful_votes_count integer,
+funny_votes_count integer,
+cool_votes_count integer,
+fans_count integer,
+average_stars_rating integer,
+
+hot_compliment_count integer,
+more_compliment_count integer,
+profile_compliment_count integer,
+cute_compliment_count integer,
+list_compliment_count integer,
+note_compliment_count integer,
+plain_compliment_count integer,
+cool_compliment_count integer,
+funny_compliment_count integer,
+write_compliment_count integer,
+photo_compliment_count integer,
+
+
+
+created_ts timestamp,
+upd_ts timestamp
+
+
+
+
+
+)
